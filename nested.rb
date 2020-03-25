9d37cfc6a1572836c57b3c37199834b777a50c8c
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-03-25 01:04:37 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -128,7 +128,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-dennis_ritchies_language = "Assembly"
-programmer_hash[:dennis_ritchie][:languages] += dennis_ritchies_language
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 programmer_hash
 end
