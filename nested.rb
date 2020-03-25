3b5562423add12235bccbcb7e8c51c98fdd939cc
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-03-25 00:58:46 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -63,11 +63,8 @@ end
 def adding_matz
 
 	programmer_hash =
+
 	{
-		  :yukihiro_matsumoto => {
-	  	  :known_for => "Ruby",
-		    :languages => ["LISP", "C"]
-	},
       :grace_hopper => {
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
@@ -81,7 +78,10 @@ def adding_matz
         :languages => ["C"]
       }
     }
-programmer_hash[:yukihiro_matsumoto]
+programmer_hash[:yukihiro_matsumoto] = {}
+programmer_hash[:yukihiro_matsumoto][:known_for] ="Ruby"
+programmer_hash[:yukihiro_matsumoto][:languages] = "LISP", "CC"
+programmer_hash
 
 end
 
